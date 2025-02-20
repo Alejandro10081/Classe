04e82e1f23dd0881d55532054ff1cf846c6c2239
Alejandro10081 <amorrom4@insgabrielamistral.cat>
2025-02-20 11:42:26 +0100

    Update 2 main.java

--- src/github_alejandro/main.java
+++ src/github_alejandro/main.java
@@ -3,5 +3,6 @@ package github_alejandro;
 public class main {
 	public static void main(String[]args) {
 			System.out.println("1a modificació Git");
+			System.out.println(“2a modificació GitHub”);
         }
     }
